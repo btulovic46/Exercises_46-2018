8485bb63c2bf5b3e27b992bcb2dd4a2a34caf19a
btulovic46 <borkotulovic9991@gmail.com>
2020-11-10 01:04:50 +0100

    Cetvrti komit - Smestanje podataka u listu ExerciseResult

--- Form1.cs
+++ Form1.cs
@@ -7,15 +7,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Drawing.Text;
 
 namespace Exercises_46-2018
 {
-    public partial class Form1 : Form
+    public partial class GlavnaForma : Form
+{
+    private string connectionString = "Data Source=(localdb)\\ProjectsV13;Initial Catalog=FacultyDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+    public GlavnaForma()
     {
-        public Form1()
-        {
             InitializeComponent();
         }
+    private void Form1_Load(object sender, EventArgs e)
+    {
+        SqlConnection sqlConnection = new SqlConnection();
+        sqlConnection.ConnectionString = connectionString;
+
+        List<ExerciseResult> exerciseResults = new List<ExerciseResult>();
+        sqlConnection.Open();
+        SqlCommand sqlCommand = new SqlCommand();
+        sqlCommand.Connection = sqlConnection;
+
+        sqlConnection.Close();
 
     }
+
+    private void Prikaz_rezultata_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+
+}
 }
